401b4b3213f3ce76f91f13ed31ac9634207cdc26
louvki <lukas.vismn@gmail.com>
2019-06-15 06:03:47 +0200

    set up project list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ProjectList from '../projects/ProjectList'
+import Notifications from './Notifications'
 
 class Dashboard extends Component {
   render() {
@@ -6,10 +8,10 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <p>p</p>
+            <ProjectList />
           </div>
           <div className="col s12 m5 offset-m1">
-          <p>p</p>
+            <Notifications />
           </div>
         </div>
       </div>
